965cba9bbf778aa9dc8e3faa52b8ccc53a049a4e
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-03-24 15:53:31 +0300

    header css done

--- client/BDU-Almuni-FrontEnd/src/component/Header.jsx
+++ client/BDU-Almuni-FrontEnd/src/component/Header.jsx
@@ -1,13 +1,20 @@
 import NavBar from "./NavBar"
-
+import '../styles/header.css'
+import background from '../assets/images/photo_2024-02-25_15-58-46.jpg'
 const Header = () => {
   return (
-    <div className='header-container'>
+    <div className="overlay">
+      <div className='header-container'>
         <NavBar />
-        <div className="wellcome-text">
-            <h1 className="top-title">Wellcome to Bahir Dar University</h1>
-            <h2 className="bottom-title">This is the official site of Bahir Dar university alumni</h2>
+        <div className="overlay"> <img className='background-img'src={background} alt="" />
+        {/* <div className="overlay"></div> */}
         </div>
+       
+        <div className="wellcome-text">
+            <p className="top-title">Wellcome to Bahir Dar University</p>
+            <p className="bottom-title">This is the official site of Bahir Dar university alumni</p>
+        </div>
+    </div>
     </div>
   )
 }
