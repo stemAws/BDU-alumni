bd7ea32b023ebcc1a6c91c3de62ef26ce71c5044
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-03-30 15:46:09 +0300

    skeleton done

--- client/BDU-Almuni-FrontEnd/src/pages/NewsAndUpdates.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/NewsAndUpdates.jsx
@@ -1,8 +1,44 @@
 
 const NewsAndUpdates = () => {
   return (
-    <div>
-        
+    <div className="news-and-updates-container">
+        <div className="nUTitle">
+          <p>News And Updates</p>
+        </div>
+        <div className="left-side">
+          <div className="big-news-img-container">
+            <div className="big-new-img">
+            <img src="" alt="" />
+            </div>
+            <div className="news-category">
+            <p>catagory</p>
+            </div>
+            <p className="news-small-detail">great news</p>
+            <p className="news-date">feb 12 - 2024</p>
+          </div>
+          <div className="headline-container">
+          <div className="headline-box">HeadLine</div>
+          <div className="headline-texts">
+            <p>he was a rich man</p>
+          </div>
+          </div>
+        </div>
+        <div className="right-side">
+        <div className="news-timing-nav">
+          <ul>
+            <li>Latest</li>
+            <li>Yesterday</li>
+            <li>Last week</li>
+          </ul>
+        </div>
+        <div className="categorized-news-container">
+        <div className="each-news">
+          <div className="small-news-img"></div>
+          <p className="news-detail">it was crazy</p>
+          <p className="news-date">feb 12 - 2024</p>
+        </div>
+        </div>
+        </div>
     </div>
   )
 }
