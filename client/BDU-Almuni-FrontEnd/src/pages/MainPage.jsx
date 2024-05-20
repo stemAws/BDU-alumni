eec3ff0bda75e63d0110a1fd3f1b7cd8ab36baa8
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-05-20 08:56:28 +0300

    news and updates

--- client/BDU-Almuni-FrontEnd/src/pages/MainPage.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/MainPage.jsx
@@ -2,20 +2,24 @@ import { useState,createContext } from 'react';
 import Header from '../component/Header'
 import Signin from '../component/Signin'
 import NewsAndUpdates from './NewsAndUpdates'
+import Stories from './Stories';
+import Events from './Events';
 export const SigninContext = createContext();
 const MainPage = () => {
   const [signin, setsignin] = useState(false);
+  const [loginState, setloginState] = useState(false);
   return (
     <div>
       <SigninContext.Provider value={{ signin, setsignin}}>
-        <Header />
+        <Header loginState={loginState}/>
         </SigninContext.Provider>
         
         <NewsAndUpdates />
         {
-          signin&&<Signin setsignin={setsignin}/>
+          signin&&<Signin setloginState={setloginState} setsignin={setsignin}/>
         }
-       
+        <Stories/>
+       <Events/>
     </div>
   )
 }
