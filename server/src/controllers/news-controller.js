ccedf409c39054a2f82be7fe5ef05cb58a0afcb0
NetsanetAlemu <netsialemu1907@gmail.com>
2024-05-24 16:30:13 +0300

    Controller to search for a news

--- server/src/controllers/news-controller.js
+++ server/src/controllers/news-controller.js
@@ -1,96 +1,128 @@
-const newsService = require('../services/news-services');
+const newsService = require("../services/news-services");
 const path = require("path");
-const sharp = require('sharp');
+const sharp = require("sharp");
 
-const { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } = require("firebase/storage");
+const {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} = require("firebase/storage");
 const firebaseConfig = require("../config/firebaseConfig");
 const firebsae = require("firebase/app");
 firebsae.initializeApp(firebaseConfig);
 const storage = getStorage();
 
 exports.createNews = async (req, res) => {
-    try {
-      const adminId = req.params.adminId;      
-      const { title, content, category } = req.body;
-      const imagePath = req.file ? `events/${Date.now()}${path.extname(req.file.originalname)}` : null;
-      let downloadURL = null;
-  
-      if (req.file) {
-        const fileRef = ref(storage, imagePath);
-        resizedFile = await sharp(req.file.buffer).jpeg({ quality: 50 }).toBuffer()
-  
-        await uploadBytes(fileRef, resizedFile);
-        downloadURL = await getDownloadURL(fileRef);
-      }
-  
-      const news = await newsService.addNews(title, content, category, downloadURL, adminId);
-      res.status(201).json({ message: "news added successfully", news });
-    } catch (error) {
-      console.error("Error adding news:", error);
-      res.status(500).json({ error: "Internal Server Error", message: error.message });
-    }
-  };
+  try {
+    const adminId = req.params.adminId;
+    const { title, content, category } = req.body;
+    const imagePath = req.file
+      ? `events/${Date.now()}${path.extname(req.file.originalname)}`
+      : null;
+    let downloadURL = null;
 
-  exports.getAllNews = async (req, res) => {
-    try {
-      const news = await newsService.getNews();
-      res.send(news);
-    } catch (error) {
-      console.error("Error fetching news:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-  exports.getNewsList = async (req, res) => {
-    try {
-      const news = await newsService.NewsList();
-      res.send(news);
-    } catch (error) {
-      console.error("Error fetching news:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-  exports.getNewsById = async (req, res) => {
-    try {
-      const news = await newsService.getANews(req.params.newsId);
-      
-      if (!news) res.status(404).json("No record by the given id");
-      else res.send(news);
-    } catch (error) {
-      console.error("Error fetching news by ID:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
+    if (req.file) {
+      const fileRef = ref(storage, imagePath);
+      resizedFile = await sharp(req.file.buffer)
+        .jpeg({ quality: 50 })
+        .toBuffer();
 
-  
+      await uploadBytes(fileRef, resizedFile);
+      downloadURL = await getDownloadURL(fileRef);
+    }
+
+    const news = await newsService.addNews(
+      title,
+      content,
+      category,
+      downloadURL,
+      adminId
+    );
+    res.status(201).json({ message: "news added successfully", news });
+  } catch (error) {
+    console.error("Error adding news:", error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
+  }
+};
+
+exports.getAllNews = async (req, res) => {
+  try {
+    const news = await newsService.getNews();
+    res.send(news);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+exports.getNewsList = async (req, res) => {
+  try {
+    const news = await newsService.NewsList();
+    res.send(news);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+exports.getNewsById = async (req, res) => {
+  try {
+    const news = await newsService.getANews(req.params.newsId);
+
+    if (!news) res.status(404).json("No record by the given id");
+    else res.send(news);
+  } catch (error) {
+    console.error("Error fetching news by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.searchNews = async (req, res) => {
+  try {
+    const { title, category } = req.body;
+    const events = await newsService.searchNewsBy(title, category);
+    res.send(events);
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+};
+
 exports.updateNews = async (req, res) => {
-    try {
-      const newsId = req.params.newsId;
-      const updatedNewstData = req.body;
-  
-      const affectedRows = await newsService.updateANews(newsId, updatedNewstData);
-  
-      if (affectedRows === 0) {
-        return res.status(404).json({ error: `No record with the given id: ${newsId}` });
-      }
-  
-      return res.json({ message: "News updated successfully" });
-    } catch (error) {
-      console.error("Error updating news:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
+  try {
+    const newsId = req.params.newsId;
+    const updatedNewstData = req.body;
 
-  exports.deleteNews = async (req, res) => {
-    try {
-      const news = await newsService.deleteANews(req.params.newsId);
-      
-      if (!news) res.status(404).json("No record by the given id");
-      else res.send(news);
-    } catch (error) {
-      console.error("Error fetching news by ID:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
+    const affectedRows = await newsService.updateANews(
+      newsId,
+      updatedNewstData
+    );
 
-  
-  
+    if (affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ error: `No record with the given id: ${newsId}` });
+    }
+
+    return res.json({ message: "News updated successfully" });
+  } catch (error) {
+    console.error("Error updating news:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.deleteNews = async (req, res) => {
+  try {
+    const news = await newsService.deleteANews(req.params.newsId);
+
+    if (!news) res.status(404).json("No record by the given id");
+    else res.send(news);
+  } catch (error) {
+    console.error("Error fetching news by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
