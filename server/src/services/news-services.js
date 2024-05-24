abcd86d26841282508f5e38552208d748e67b485
NetsanetAlemu <netsialemu1907@gmail.com>
2024-05-24 15:36:20 +0300

    Service to get a single news

--- server/src/services/news-services.js
+++ server/src/services/news-services.js
@@ -1,29 +1,16 @@
 const db = require("../config/db");
 
-exports.addNews = async (
-  title,
-  content,
-  category,
-  image_path,
-  adminId
-) => {
+exports.addNews = async (title, content, category, image_path, adminId) => {
   try {
     let query, params;
 
     if (image_path) {
       query =
         "INSERT INTO news (title, content,  category, imagePath, adminId) VALUES (?, ?, ?, ?, ?)";
-      params = [
-        title,
-        content,
-        anouncementDate,
-        category,
-        image_path,
-        adminId
-      ];
+      params = [title, content, anouncementDate, category, image_path, adminId];
     } else {
       query =
-      "INSERT INTO news (title, content, category, adminId) VALUES (?, ?, ?, ?)";
+        "INSERT INTO news (title, content, category, adminId) VALUES (?, ?, ?, ?)";
       params = [title, content, category, adminId];
     }
 
@@ -37,7 +24,20 @@ exports.addNews = async (
 };
 
 exports.getNews = async () => {
-  const [events] =
-    await db.query(`SELECT *, DATE_FORMAT(createdAt, '%Y-%m-%d') AS createdAt, DATE_FORMAT(updatedAt, '%Y-%m-%d') AS updatedAt FROM news`);
-  return events;
+  const [news] = await db.query(
+    `SELECT *, DATE_FORMAT(createdAt, '%Y-%m-%d') AS createdAt, DATE_FORMAT(updatedAt, '%Y-%m-%d') AS updatedAt FROM news`
+  );
+  
+  return news;
 };
+
+exports.getANews = async (newsId) => {
+  
+  const [news] = await db.query(
+    `SELECT title, content, category, DATE_FORMAT(createdAt, '%Y-%m-%d') AS createdAt FROM news WHERE newsId = ?`,
+    [newsId]
+  );
+  
+  return news.length > 0 ? news[0]: null;
+};
+
