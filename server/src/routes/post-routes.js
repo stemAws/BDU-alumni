aabb0064eee27f46b0f6bb4f130bcf7b1a78add3
NetsanetAlemu <netsialemu1907@gmail.com>
2024-05-25 11:56:00 +0300

    updated route for post

--- server/src/routes/post-routes.js
+++ server/src/routes/post-routes.js
@@ -6,12 +6,12 @@ const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/posts/:alumniID", upload.single("image"), postController.createPost);
+router.post("/posts/:alumniId", upload.single("image"), postController.createPost);
 router.get("/posts", postController.getAllPosts);
 router.get("/addedPosts", postController.getAddedStories);
-router.put("/addedPosts/:id", postController.updateSuggestedByAdmin);
-router.get("/posts/:idOrUsername", postController.getPostsByUsernameOrId);
-router.delete("/posts/:id", postController.deletePost);
-router.put("/posts/:id", postController.updatePost);
+router.put("/addedPosts/:postId", postController.updateSuggestedByAdmin);
+router.get("/posts/:alumniIdOrUsername", postController.getPostsByUsernameOrId);
+router.delete("/posts/:postId", postController.deletePost);
+router.put("/posts/:postId", postController.updatePost);
 
 module.exports = router;
