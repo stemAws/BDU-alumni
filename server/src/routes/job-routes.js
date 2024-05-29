37a7059cd2f1a2d08039ffa87603323136b14f04
NetsanetAlemu <netsialemu1907@gmail.com>
2024-05-29 23:43:23 +0300

    Route to update a job

--- server/src/routes/job-routes.js
+++ server/src/routes/job-routes.js
@@ -4,13 +4,9 @@ const jobController = require("../controllers/job-controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post(
-  "/add-job",
-  upload.single("image"),
-  jobController.createJob
-);
+router.post("/add-job", upload.single("image"), jobController.createJob);
 router.get("/job-list", jobController.getAdminJobs);
 router.get("/job/:jobId", jobController.getJobById);
-
+router.put("/update-job/:jobId", jobController.updateJobById);
 
 module.exports = router;
