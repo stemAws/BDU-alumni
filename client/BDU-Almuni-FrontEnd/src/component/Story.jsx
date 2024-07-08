878bd2c08ab864b6598b8eeb1fbfb2fb207ed22f
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-07-08 14:46:39 +0300

    featured stories fetched

--- client/BDU-Almuni-FrontEnd/src/component/Story.jsx
+++ client/BDU-Almuni-FrontEnd/src/component/Story.jsx
@@ -5,7 +5,7 @@ const Story = ({story}) => {
     <div className="each-story">
                 <img className="story-img"src={story.img} alt="" />
             <div className="story-description-container">
-                <p className="story-text">{story.description}</p>
+                <p className="story-text">{story.content}</p>
                 <p className="story-readmore">
                     Read more <FaArrowRight/>
                 </p>
