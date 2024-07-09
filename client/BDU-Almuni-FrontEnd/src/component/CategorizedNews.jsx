31149aa60e83a72616371fb1e13b33838e05b1ff
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-07-09 15:06:25 +0300

    select from categoried news to pop in to the big screen(today news)

--- client/BDU-Almuni-FrontEnd/src/component/CategorizedNews.jsx
+++ client/BDU-Almuni-FrontEnd/src/component/CategorizedNews.jsx
@@ -1,6 +1,13 @@
-const CategorizedNews = ({news}) => {
+const CategorizedNews = ({news,newsClicked}) => {
+  const handleId=(id)=>{
+    newsClicked(id)
+    document.querySelectorAll(`.each-news`).forEach((news)=>{
+      news.classList.remove("clicked")
+    })
+    document.querySelector(`.each-news.n${id}`).classList.add("clicked")
+  }
   return (
-    <div className="each-news">
+    <div onClick={()=>handleId(news.newsId)} className={`each-news n${news.newsId}`}>
           <div className="small-news-img">
             <img src={news.img} alt="" />
           </div>
