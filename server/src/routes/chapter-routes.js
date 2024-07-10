59183ce955f60d8205821ce061d94920282b94fd
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-10 11:58:26 +0300

    Route to delete a chapter

--- server/src/routes/chapter-routes.js
+++ server/src/routes/chapter-routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const chapterController = require("../controllers/chapter-controller");
 
 router.post("/add-chapter", chapterController.createChapter);
-router.get("/chapters-list", chapterController.getChaptersList);
+router.get("/list-chapters", chapterController.getChaptersList);
 router.get("/get-chapter/:chapterId", chapterController.getChapterById);
 router.put("/edit-chapter/:chapterId", chapterController.updateChapter);
+router.delete("/delete-chapter/:chapterId", chapterController.deleteChapter);
+
 
 
 
