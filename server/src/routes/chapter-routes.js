139dfb6a436bb11b7749e16f5fece068be46c1ec
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-10 11:53:09 +0300

    Route to update a chapter

--- server/src/routes/chapter-routes.js
+++ server/src/routes/chapter-routes.js
@@ -5,6 +5,7 @@ const chapterController = require("../controllers/chapter-controller");
 router.post("/add-chapter", chapterController.createChapter);
 router.get("/chapters-list", chapterController.getChaptersList);
 router.get("/get-chapter/:chapterId", chapterController.getChapterById);
+router.put("/edit-chapter/:chapterId", chapterController.updateChapter);
 
 
 
