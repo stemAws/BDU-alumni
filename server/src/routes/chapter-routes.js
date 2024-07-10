16e0b9de39aff35683a76abd6b3c00fb8aace5e7
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-10 11:45:45 +0300

    Route to get a single chapter

--- server/src/routes/chapter-routes.js
+++ server/src/routes/chapter-routes.js
@@ -4,6 +4,7 @@ const chapterController = require("../controllers/chapter-controller");
 
 router.post("/add-chapter", chapterController.createChapter);
 router.get("/chapters-list", chapterController.getChaptersList);
+router.get("/get-chapter/:chapterId", chapterController.getChapterById);
 
 
 
