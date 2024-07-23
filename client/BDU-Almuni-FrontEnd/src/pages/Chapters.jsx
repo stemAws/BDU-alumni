dfb06b4979d5f6e8ccab4ddd7e2ccdbf823ca1c1
yordanos ayenew <yordanosayenew54@gmail.com>
2024-07-23 10:07:07 -0700

    chapters

--- client/BDU-Almuni-FrontEnd/src/pages/Chapters.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/Chapters.jsx
@@ -12,20 +12,20 @@ const Chapters = () => {
         <div className='ind-chapters-cont'>
         <div className='ind-chapters'>
            <h4>club1</h4>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut. ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse repudiandae accusamus ducimus reiciendis nam omnis excepturi recusandae velit error.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut., ipsum dolor sit amet consectetur adipisicing elit. Aut impedit suscipit neque eligendi enim vitae cumque quia aliquid officiis at.</p>
-           <button>JOIN</button>
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, enim natus officiis ipsum magni architecto? Nesciunt iure velit numquam quas nobis minima a ad tempora ut maiores. Blanditiis inventore necessitatibus pariatur placeat veritatis voluptatibus molestias quaerat obcaecati delectus vel ad temporibus, ut mollitia dicta labore. Eveniet magni velit at sed.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut. ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse repudiandae accusamus ducimus reiciendis nam omnis excepturi recusandae velit error.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut., ipsum dolor sit amet consectetur adipisicing elit. Aut impedit suscipit neque eligendi enim vitae cumque quia aliquid officiis at.</p>
+           <div><button>JOIN</button></div>
+        </div>
+
+        <div className='ind-chapters'>
+           <h4>club1</h4>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat repudiandae nam, vitae blanditiis facilis veritatis beatae quibusdam pariatur itaque? Incidunt itaque voluptatum quo delectus blanditiis adipisci dolorum quidem ad error.Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt tempora, id cupiditate facere ipsam itaque, nostrum praesentium error numquam perspiciatis placeat dolorem ducimus amet, modi dolores illo sed corrupti? Perspiciatis, officiis delectus. Odit, dolor? Libero aperiam illo iusto iste officia inventore in hic repudiandae recusandae quaerat consectetur, maxime possimus veritatis!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut. ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse repudiandae accusamus ducimus reiciendis nam omnis excepturi recusandae velit error.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut., ipsum dolor sit amet consectetur adipisicing elit. Aut impedit suscipit neque eligendi enim vitae cumque quia aliquid officiis at.</p>
+           <div><button>JOIN</button></div>
         </div>
 
         <div className='ind-chapters'>
            <h4>club1</h4>
            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut. ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse repudiandae accusamus ducimus reiciendis nam omnis excepturi recusandae velit error.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut., ipsum dolor sit amet consectetur adipisicing elit. Aut impedit suscipit neque eligendi enim vitae cumque quia aliquid officiis at.</p>
-           <button>JOIN</button>
-        </div>
-
-        <div className='ind-chapters'>
-           <h4>club1</h4>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut. ipsum dolor sit amet consectetur adipisicing elit. Distinctio esse repudiandae accusamus ducimus reiciendis nam omnis excepturi recusandae velit error.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, iste culpa. Nam totam hic est, consequatur distinctio ea sequi ut., ipsum dolor sit amet consectetur adipisicing elit. Aut impedit suscipit neque eligendi enim vitae cumque quia aliquid officiis at.</p>
-           <button>JOIN</button>
+           <div><button>JOIN</button></div>
         </div>
         </div>
     
