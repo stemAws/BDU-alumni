39f6a00a9f663a40acaadb1335d1446351d50cc4
a-abrham <abrshkorra@gmail.com>
2024-07-26 14:41:25 +0300

    story ui modification

--- client/BDU-Almuni-FrontEnd/src/component/Story.jsx
+++ client/BDU-Almuni-FrontEnd/src/component/Story.jsx
@@ -1,19 +1,17 @@
 import { FaArrowRight } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
-const Story = ({story}) => {
+const Story = ({ story }) => {
   return (
     <Link to={`stories/${story.postId}`}>
-    <div className="each-story">
-              {story.mediaPath&&(<img className="story-img"src={story.mediaPath} alt="" />) } 
-            <div className={`${story.mediaPath?"story-description-container":"story-description-container without"}`}>
-                <p className="story-text">{story.content}</p>
-                    <p className="story-readmore">
-                        Read more <FaArrowRight/>
-                    </p>
-            </div>
+      <div className="each-story">
+        <img className="story-img" src={story.mediaPath} alt="" />
+        <div className="story-description-container">
+          <p className="story-text">{story.content}</p>
+          <p className="story-readmore">Read more <FaArrowRight /></p>
         </div>
-        </Link>
+      </div>
+    </Link>
   )
 }
 
