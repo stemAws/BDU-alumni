d16d11961b20be9090003a78d926e9e27fb08f52
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-29 13:15:42 +0300

    Renamed sendAt

--- server/src/services/feedback-services.js
+++ server/src/services/feedback-services.js
@@ -1,31 +1,34 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 exports.addFeedback = async (fullName, email, message) => {
-    try {
-        const [result] = await db.query(
-            'INSERT INTO feedback (fullName, email, message) VALUES (?, ?, ?)',
-            [fullName, email, message]
-        );
+  try {
+    const [result] = await db.query(
+      "INSERT INTO feedback (fullName, email, message) VALUES (?, ?, ?)",
+      [fullName, email, message]
+    );
 
-        return result.insertId;
-    } catch (error) {
-        console.error('Error adding feedback:', error);
-        throw error;
-    }
+    return result.insertId;
+  } catch (error) {
+    console.error("Error adding feedback:", error);
+    throw error;
+  }
 };
 
 exports.getAllfeedback = async () => {
-    const [feedback] = await db.query(`SELECT *, DATE_FORMAT(sendAt, '%Y-%m-%d') AS sendAt
-    FROM feedback`);    
-    return feedback;
+  const [feedback] =
+    await db.query(`SELECT *, DATE_FORMAT(submittedAt, '%Y-%m-%d') AS submittedAt
+    FROM feedback`);
+  return feedback;
 };
 
 exports.deleteFeedback = async (feedBackID) => {
-    const [result] = await db.query("DELETE FROM feedback WHERE feedBackID = ?", [feedBackID]);
+  const [result] = await db.query("DELETE FROM feedback WHERE feedBackID = ?", [
+    feedBackID,
+  ]);
 
-    if (result.affectedRows === 0) {
-        return { success: false, message: 'No record by the given id' };
-    }
+  if (result.affectedRows === 0) {
+    return { success: false, message: "No record by the given id" };
+  }
 
-    return { success: true, message: 'Feedback deleted successfully' };
+  return { success: true, message: "Feedback deleted successfully" };
 };
