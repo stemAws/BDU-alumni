64b355c7bcc8eee13173db9d54f6a21ce62c8435
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-30 11:09:11 +0300

    modified routes of gallery

--- client/BDU-Almuni-FrontEnd/src/pages/Admin.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/Admin.jsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import SideBar from '../component/SideBar';
-import Home from './AdminPages/Home';
-import '../styles/Admin.css';
-import Users from './AdminPages/Users';
-import Stories from '../pages/AdminPages/stories';
-import EventList from './AdminPages/EventList';
-import FeedBack from './AdminPages/FeedBack';
-import AddEvent from './AdminPages/AddEvent';
-import AddedStory from './AdminPages/AddedStory';
-import DonationList from './AdminPages/DonationList';
-import AddDonation from './AdminPages/AddDonation';
-import AddUser from './AdminPages/AddUser';
-import News from './AdminPages/News';
-import NewsList from './AdminPages/NewsList';
-import Chapters from './AdminPages/Chapters';
-import JobOffer from './AdminPages/JobOffer';
-import SuggestedJob from './AdminPages/SuggestedJob';
-import AGallery from './AdminPages/AGallery';
-import AddGallery from './AdminPages/AddGallery';
-import AdminSignin from '../component/AdminSignin';
-import AuthService from '../component/AuthService';
-import ChaptersList from './AdminPages/chaptersList';
-import Editnews from './AdminPages/Editnews'; 
-import EditDonation from './AdminPages/EditDonation'; 
+import { useEffect, useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import SideBar from "../component/SideBar";
+import Home from "./AdminPages/Home";
+import "../styles/Admin.css";
+import Users from "./AdminPages/Users";
+import Stories from "../pages/AdminPages/stories";
+import EventList from "./AdminPages/EventList";
+import FeedBack from "./AdminPages/FeedBack";
+import AddEvent from "./AdminPages/AddEvent";
+import AddedStory from "./AdminPages/AddedStory";
+import DonationList from "./AdminPages/DonationList";
+import AddDonation from "./AdminPages/AddDonation";
+import AddUser from "./AdminPages/AddUser";
+import News from "./AdminPages/News";
+import NewsList from "./AdminPages/NewsList";
+import Chapters from "./AdminPages/Chapters";
+import JobOffer from "./AdminPages/JobOffer";
+import SuggestedJob from "./AdminPages/SuggestedJob";
+// import AGallery from './AdminPages/AGallery';
+import AddGallery from "./AdminPages/AddGallery";
+import AdminSignin from "../component/AdminSignin";
+import AuthService from "../component/AuthService";
+import ChaptersList from "./AdminPages/chaptersList";
+import Editnews from "./AdminPages/Editnews";
+import EditDonation from "./AdminPages/EditDonation";
+import GalleryList from "./AdminPages/GalleryList";
+import EditGallery from "./AdminPages/EditGallery";
 
 const Admin = () => {
   const [isAdminAuthenticated, setIsAdminAuthenticated] = useState(false);
 
   useEffect(() => {
-    if (AuthService.isAuthenticated('admin')) {
+    if (AuthService.isAuthenticated("admin")) {
       setIsAdminAuthenticated(true);
     } else {
       setIsAdminAuthenticated(false);
@@ -42,32 +44,33 @@ const Admin = () => {
         <div className="admincontainer">
           <SideBar />
           <Routes>
-            <Route path='home' element={<Home />} />
-            <Route path='users' element={<Users />} />
-            <Route path='News' element={<NewsList />} />
+            <Route path="home" element={<Home />} />
+            <Route path="users" element={<Users />} />
+            <Route path="News" element={<NewsList />} />
             <Route path="/admin/news/:newsId" element={<Editnews />} />
-            <Route path='AddNews' element={<News />} />
-            <Route path='AddUser' element={<AddUser />} />
-            <Route path='story' element={<Stories />} />
-            <Route path='addedStories' element={<AddedStory />} />
-            <Route path='Events' element={<EventList />} />
-            <Route path='gallery' element={<AGallery />} />
-            <Route path='Addgallery' element={<AddGallery />} />
-            <Route path='AddEvent' element={<AddEvent />} />
-            <Route path='jobOffer' element={<JobOffer />} />
-            <Route path='SuggestedJob' element={<SuggestedJob />} />
-            <Route path='chapters' element={<ChaptersList />} />
-            <Route path='AddChapter' element={<Chapters />} />
-            <Route path='feedback' element={<FeedBack />} />
-            <Route path='Donation' element={<DonationList />} />
+            <Route path="AddNews" element={<News />} />
+            <Route path="AddUser" element={<AddUser />} />
+            <Route path="story" element={<Stories />} />
+            <Route path="addedStories" element={<AddedStory />} />
+            <Route path="Events" element={<EventList />} />
+            <Route path="gallery" element={<GalleryList />} />
+            <Route path="AddGallery" element={<AddGallery />} />
+            <Route path="EditGallery" element={<EditGallery />} />
+            <Route path="AddEvent" element={<AddEvent />} />
+            <Route path="jobOffer" element={<JobOffer />} />
+            <Route path="SuggestedJob" element={<SuggestedJob />} />
+            <Route path="chapters" element={<ChaptersList />} />
+            <Route path="AddChapter" element={<Chapters />} />
+            <Route path="feedback" element={<FeedBack />} />
+            <Route path="Donation" element={<DonationList />} />
             <Route path="/admin/donation/:id" element={<EditDonation />} />
-            <Route path='AddDonation' element={<AddDonation />} />
-            <Route path='*' element={<Navigate to="home" />} />
+            <Route path="AddDonation" element={<AddDonation />} />
+            <Route path="*" element={<Navigate to="home" />} />
           </Routes>
         </div>
       ) : (
         <Routes>
-          <Route path='*' element={<AdminSignin />} />
+          <Route path="*" element={<AdminSignin />} />
         </Routes>
       )}
     </>
