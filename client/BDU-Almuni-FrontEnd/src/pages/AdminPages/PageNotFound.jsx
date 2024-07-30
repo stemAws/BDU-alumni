2ff11b7fe228581a50b651126f0e40ee197cc632
yordanos ayenew <yordanosayenew54@gmail.com>
2024-07-29 22:21:01 -0700

    adminpage not found

--- client/BDU-Almuni-FrontEnd/src/pages/AdminPages/PageNotFound.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/AdminPages/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import { Fa500Px } from 'react-icons/fa'
-import Button from '../component/Button'
-import '../styles/pageNotFound.css'
+import Button from '../../component/Button'
+import '../../styles/pageNotFound.css'
 import {Link} from 'react-router-dom'
 const PageNotFound = () => {
   return (
@@ -8,7 +8,7 @@ const PageNotFound = () => {
         <Fa500Px size={50} />
         <p className='four04'>404</p>
          <p>Sorry, Page Not Found</p>
-        <Link to='/'><Button text={'Home'}/></Link> 
+        <Link to='/admin/home'><Button text={'Home'}/></Link> 
             </div>
   )
 }
