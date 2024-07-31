bc02f73d223f52deb74bae2f602a9d2911ca3c2d
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-31 07:59:00 +0300

    Removed department while creating gallery in media services

--- server/src/services/media-services.js
+++ server/src/services/media-services.js
@@ -9,8 +9,8 @@ exports.createGallery = async ({
 }) => {
   try {
     const [result] = await db.query(
-      "INSERT INTO Gallery (media, title, year, department, description) VALUES (?,?,?, ?, ?)",
-      [JSON.stringify(images), event, year, department, description]
+      "INSERT INTO Gallery (media, title, year,  description) VALUES (?,?,?, ?)",
+      [JSON.stringify(images), event, year, description]
     );
 
     return result.insertId;
@@ -22,7 +22,7 @@ exports.createGallery = async ({
 exports.getGalleryById = async (gID) => {
   try {
     const result = await db.query(
-      "SELECT galleryId as galleryID, title as event, description, year, department, media as images FROM Gallery WHERE galleryId = ?",
+      "SELECT galleryId as galleryID, title as event, description, year,media as images FROM Gallery WHERE galleryId = ?",
       [gID]
     );
     return result[0][0];
@@ -34,7 +34,7 @@ exports.getGalleryById = async (gID) => {
 exports.getAllGallery = async () => {
   try {
     const [result] = await db.query(
-      "SELECT galleryId as galleryID, title as event, description, year, department, media as images FROM Gallery"
+      "SELECT galleryId as galleryID, title as event, description, year, media as images FROM Gallery"
     );
     return result;
   } catch (error) {
@@ -54,12 +54,12 @@ exports.deleteGallery = async (galleryID) => {
 };
 
 exports.updateGallery = async (galleryID, updatedGallery) => {
-  const { event, year, department, description } = updatedGallery;
+  const { event, year, description } = updatedGallery;
 
   try {
     const [result] = await db.query(
-      "UPDATE Gallery SET title = ?, year = ? , department = ?,  description = ? WHERE galleryID = ?",
-      [event, year, department, description, galleryID]
+      "UPDATE Gallery SET title = ?, year = ? ,   description = ? WHERE galleryID = ?",
+      [event, year, description, galleryID]
     );
     return result.affectedRows;
   } catch (error) {
