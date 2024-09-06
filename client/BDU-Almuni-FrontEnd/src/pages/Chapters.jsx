5da48d6d3e95068e383c477d268afe2136b94170
NetsanetAlemu <netsialemu1907@gmail.com>
2024-09-06 09:41:34 +0300

    Modified background style

--- client/BDU-Almuni-FrontEnd/src/pages/Chapters.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/Chapters.jsx
@@ -6,12 +6,12 @@ const Chapters = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [chaptersToShow, setchaptersToShow] = useState(3);
-  const handleSeemore=()=>{
-    setchaptersToShow(chaptersToShow + 3)
-}
-const handleSeeless=()=>{
-  setchaptersToShow(chaptersToShow - 3)
-}
+  const handleSeemore = () => {
+    setchaptersToShow(chaptersToShow + 3);
+  };
+  const handleSeeless = () => {
+    setchaptersToShow(chaptersToShow - 3);
+  };
   useEffect(() => {
     const fetchChapters = async () => {
       try {
@@ -40,48 +40,62 @@ const handleSeeless=()=>{
   return (
     <div className="chapters-container">
       <div className="chapters-title">
-        <div className="circle-bg"></div>
+        {/* <div className="circle-bg"></div> */}
         <p>
-          <span className="blue-text">Chapters</span>
+          <span className="blue-text">Clubs</span>
         </p>
       </div>
-      <div className="circle-bg3" />
-      <div className="circle-bg4" />
-      <div className="circle-bg5" />
+      {/* <div className="circle-bg3" /> */}
+      {/* <div className="circle-bg4" />
+      <div className="circle-bg5" /> */}
       <div className="ind-chapters-cont">
         {chapters.length === 0 ? (
           <div className="no-chapters">
             <p>No chapters posted</p>
           </div>
         ) : (
-          chapters.slice(-chaptersToShow).reverse().map((chapter, index) => (
-            <div key={index} className="ind-chapters">
-              <h4>{chapter.chapterName}</h4>
-              <p>{chapter.description}</p>
-              {chapter.website && (
-                <a
-                  href={chapter.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button
-                    type="button"
-                    className="btn-btn"
-                    onClick={() => window.open(chapter.website, "_blank")}
+          chapters
+            .slice(-chaptersToShow)
+            .reverse()
+            .map((chapter, index) => (
+              <div key={index} className="ind-chapters">
+                <h4>{chapter.chapterName}</h4>
+                <p>{chapter.description}</p>
+                {chapter.website && (
+                  <a
+                    href={chapter.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    Join
-                  </button>
-                </a>
-              )}
-            </div>
-          ))
+                    <button
+                      type="button"
+                      className="btn-btn"
+                      onClick={() => window.open(chapter.website, "_blank")}
+                    >
+                      Join
+                    </button>
+                  </a>
+                )}
+              </div>
+            ))
         )}
-        
-      </div> 
+      </div>
       <div className="buttons">
-       {chaptersToShow < chapters.length && <Button className={`see-more`} onClick={()=>handleSeemore()} text={"More"} />}
-       {(chaptersToShow > 3 ) && <Button className={`see-less`} onClick={()=>handleSeeless()} text={"Less"} />}
-       </div>
+        {chaptersToShow < chapters.length && (
+          <Button
+            className={`see-more`}
+            onClick={() => handleSeemore()}
+            text={"More"}
+          />
+        )}
+        {chaptersToShow > 3 && (
+          <Button
+            className={`see-less`}
+            onClick={() => handleSeeless()}
+            text={"Less"}
+          />
+        )}
+      </div>
     </div>
   );
 };
