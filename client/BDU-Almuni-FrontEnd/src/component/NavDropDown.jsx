2ced5274c7d8b1ab62c6b05bf8d50168bcedf762
hailemichael-ayana <hmichaelmulugeta@gmail.com>
2024-09-30 17:10:07 +0300

    responsiveness on abtDevs

--- client/BDU-Almuni-FrontEnd/src/component/NavDropDown.jsx
+++ client/BDU-Almuni-FrontEnd/src/component/NavDropDown.jsx
@@ -5,7 +5,7 @@ const NavDropDown = ({sideMenu,onMouseEnter,onMouseLeave,className}) => {
     <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className={className}>
         <ul className="each-list-container">
             <li onClick={sideMenu&&sideMenu}><Link to ="/history"  className="each-list"><FaHistory/>History</Link></li>
-            <li onClick={sideMenu&&sideMenu}><Link to ="/aboutdevs"  className="each-list"><FaLaptopCode/>About the Dev's</Link></li>
+            <li onClick={sideMenu&&sideMenu}><Link to ="/aboutdevs"  className="each-list"><FaLaptopCode/>About the Devs</Link></li>
         </ul>
     </div>
   )
