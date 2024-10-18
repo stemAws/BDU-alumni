136fbeb7e5a6a74fffb60f803daa456c36113cf3
a-abrham <abrshkorra@gmail.com>
2024-10-18 04:50:10 -0700

    fixed

--- server/src/controllers/google-controller.js
+++ server/src/controllers/google-controller.js
@@ -1,6 +1,7 @@
 const passport = require('../config/passport-config');
 const db = require('../config/db');
 const jwt = require("jsonwebtoken");
+const alumniService = require("../services/user-services");
 
 
 exports.authenticateGoogle = passport.authenticate('google', { scope: ['profile', 'email'] });
@@ -14,15 +15,21 @@ exports.failed = async (req, res) => {
 exports.profile = async (req, res) => {
     console.log(req.user.emails[0].value)
     try {
-        const [result] = await db.query('SELECT personId FROM Person WHERE email = ?', [req.user.emails[0].value]);
-        let token = result[0].personId
+        const [result] = await db.query('SELECT username, personId FROM Person WHERE email = ?', [req.user.emails[0].value]);
+        console.log(result)
+        const id = result[0].personId
+        const token = await alumniService.getAlumniProfile(result[0].username);
 
         if (result.length > 0) {
             const realToken = jwt.sign({token}, process.env.secretKey, {
                 expiresIn: "30d",
             });
+
+            const isProd = process.env.NODE_ENV === 'prod';
+            const httpOnlyFlag = isProd;
+
       
-            res.cookie('token', realToken, { httpOnly: true }).cookie('id', token, { httpOnly: true });
+            res.cookie('token', realToken, { httpOnly: httpOnlyFlag }).cookie('id', id, { httpOnly: httpOnlyFlag });
 
             res.redirect(`http://localhost:5173/`);
         }
