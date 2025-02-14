b5c9acc5a017e6b7fd1aaacf1c0e721533580c14
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-14 23:40:03 +0300

    modified session secret

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const {notFound, errorHandler} = require('./src/middleware/error-middleware')
+const express = require("express");
+const cors = require("cors");
+const { notFound, errorHandler } = require("./src/middleware/error-middleware");
 const cookieParser = require("cookie-parser");
-const session = require('express-session');
-const passport = require('./src/config/passport-config')
+const session = require("express-session");
+const passport = require("./src/config/passport-config");
 
-
-PORT = process.env.PORT || 3005,
-
-app = express();
+(PORT = process.env.PORT || 3005), (app = express());
 app.use(cookieParser());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://geo-marked-users.netlify.app"],
@@ -21,32 +18,30 @@ app.use(
   })
 );
 
-
 app.use(
   session({
-    secret: 'your-secret-key',
+    secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', require('./src/routes/admin-routes'));
-app.use('/', require('./src/routes/donation-routes'));
-app.use('/', require('./src/routes/education-routes'));
-app.use('/', require('./src/routes/event-routes'));
-app.use('/', require('./src/routes/experience-routes'));
-app.use('/', require('./src/routes/feedback-routes'));
-app.use('/', require('./src/routes/google-routes'));
-app.use('/', require('./src/routes/media-routes'));
-app.use('/', require('./src/routes/post-routes'));
-app.use('/', require('./src/routes/user-routes'));
-app.use('/', require('./src/routes/news-routes'));
-app.use('/', require('./src/routes/job-routes'));
-app.use('/', require('./src/routes/chapter-routes'));
-
+app.use("/", require("./src/routes/admin-routes"));
+app.use("/", require("./src/routes/donation-routes"));
+app.use("/", require("./src/routes/education-routes"));
+app.use("/", require("./src/routes/event-routes"));
+app.use("/", require("./src/routes/experience-routes"));
+app.use("/", require("./src/routes/feedback-routes"));
+app.use("/", require("./src/routes/google-routes"));
+app.use("/", require("./src/routes/media-routes"));
+app.use("/", require("./src/routes/post-routes"));
+app.use("/", require("./src/routes/user-routes"));
+app.use("/", require("./src/routes/news-routes"));
+app.use("/", require("./src/routes/job-routes"));
+app.use("/", require("./src/routes/chapter-routes"));
 
 app.use(notFound);
 app.use(errorHandler);
