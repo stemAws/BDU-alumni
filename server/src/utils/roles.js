e68c1875c84ed4f38b6121dbaad6be06aaff6c2d
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-14 23:36:10 +0300

    List of roles

---
+++ server/src/utils/roles.js
@@ -0,0 +1,6 @@
+const all = ["systemAdmin", "contentManager", "alumni"];
+const admin = ["systemAdmin", "contentManager"];
+const alumni = ["alumni"];
+const sysAdmin = ["systemAdmin"];
+
+module.exports = { all, admin, alumni, sysAdmin };
