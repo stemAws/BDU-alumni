2e206cf82a7d5a5f4a9f2adabef700358d9dba7c
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-15 15:02:43 +0300

    modified role based access

--- server/src/routes/user-routes.js
+++ server/src/routes/user-routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserController = require("../controllers/user-controller");
 const multer = require("multer");
 const { verifyToken, authRoles } = require("../middleware/auth-middleware");
+const { alumni } = require("../utils/roles");
 
 const storageEngine = multer.memoryStorage();
 const upload = multer({ storage: storageEngine });
@@ -18,13 +19,13 @@ router.post("/addUser", UserController.addUser);
 router.get(
   "/alumni/:id",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.getAlumniProfile
 );
 router.get(
   "/alumni",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.getAllAlumni
 );
 router.delete("/alumni/:id", UserController.deleteAlumni); // needs a MW that will check if the requster is admin or not.
@@ -32,51 +33,51 @@ router.delete("/alumni/:id", UserController.deleteAlumni); // needs a MW that wi
 router.post(
   "/uploadProfilePicture",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   upload.single("profilePicture"),
   UserController.uploadProfilePicture
 );
 router.post(
   "/uploadCoverPicture",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   upload.single("coverPicture"),
   UserController.uploadCoverPicture
 );
 router.get(
   "/getProfilePicture/:idOrUsername",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.getProfilePicture
 );
 router.get(
   "/getCoverPicture/:idOrUsername",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.getCoverPicture
 );
 router.put(
   "/alumni",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.updateAlumni
 );
 router.post(
   "/check-username",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.checkUsernameAvailability
 );
 router.post(
   "/check-email",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.checkEmailAvailability
 );
 router.put(
   "/change-password/:alumniID",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.changePassword
 );
 router.get("/notable", UserController.getNotableAlumni);
@@ -85,16 +86,21 @@ router.post("/reset-password", UserController.resetPassword);
 router.post(
   "/confirm-password-change",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.confirmPasswordChange
 );
 router.put("/updateCustom/:alumniId", UserController.updateCustomSetting);
-router.post("/alumni-directory", UserController.searchAlumni);
+router.post(
+  "/alumni-directory",
+  verifyToken,
+  authRoles(alumni),
+  UserController.searchAlumni
+);
 
 router.get(
   "/reserve-transcript",
   verifyToken,
-  authRoles("alumni"),
+  authRoles(alumni),
   UserController.reserveTranscriptPlace
 );
 
