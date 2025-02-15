f3a74e66a35215fb648b2ca303975fef6e99c492
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-15 18:33:30 +0300

    render based on role

--- client/BDU-Almuni-FrontEnd/src/pages/Admin.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/Admin.jsx
@@ -30,16 +30,11 @@ import EditClub from "./AdminPages/EditClub";
 import AGallery from "./AdminPages/AGallery";
 import RequestedTranscript from "./AdminPages/TranscriptList";
 
-import ProtectedRoute from "../component/ProtectedRoute";
-import { Navigate } from "react-router-dom";
 const Admin = () => {
-  const { isAuthenticated } = AuthService.useAuth(); // Use the method from AuthService
+  const { isAuthenticated, role } = AuthService.useAuth(); // Use the method from AuthService
   const [isAdminAuthenticated, setIsAdminAuthenticated] = useState(null); // Start with null
-  console.log("Authentication status:", isAuthenticated);
 
   useEffect(() => {
-    console.log("useEffect triggered. isAuthenticated:", isAuthenticated);
-
     setIsAdminAuthenticated(isAuthenticated);
   }, [isAuthenticated]);
 
@@ -47,61 +42,38 @@ const Admin = () => {
     return <div>Loading...</div>; // Prevents flicker
   }
 
-  console.log("Admin component rendered");
-  console.log("isAdminAuthenticated:", isAdminAuthenticated);
-
   return (
     <>
-      {isAdminAuthenticated ? (
+      {(isAdminAuthenticated && role === "contentManager") ||
+      role === "systemAdmin" ? (
         <div className="admincontainer">
           <SideBar />
           <Routes>
-            <Route path="/admin/home" element={<Home />} />
-
-            <Route
-              path="/admin/users"
-              element={
-                <ProtectedRoute allowedRoles={["systemAdmin"]}>
-                  <Users />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route path="/admin/News" element={<NewsList />} />
-            <Route
-              path="/admin/News/edit-news/:newsId"
-              element={<Editnews />}
-            />
-            <Route path="/admin/AddNews" element={<News />} />
-            <Route path="/admin/AddUser" element={<AddUser />} />
-            <Route path="/admin/story" element={<Stories />} />
-            <Route path="/admin/addedStories" element={<AddedStory />} />
-            <Route path="/admin/Events" element={<EventList />} />
-            <Route path="/admin/adminEvents/:id" element={<EditEvent />} />
-            <Route
-              path="/admin/admin/gallery/:galleryID"
-              element={<AGallery />}
-            />
-            <Route path="/admin/gallery" element={<GalleryList />} />
-            <Route
-              path="/admin/edit-gallery/:galleryID"
-              element={<EditGallery />}
-            />
-            <Route path="/admin/Addgallery" element={<AddGallery />} />
-            <Route path="/admin/AddEvent" element={<AddEvent />} />
-            <Route path="/admin/jobOffer" element={<JobOffer />} />
-            <Route path="/admin/SuggestedJob" element={<SuggestedJob />} />
-            <Route path="/admin/chapters" element={<ChaptersList />} />
-            <Route
-              path="/admin/chapters/chapters/:chapterId"
-              element={<EditClub />}
-            />
-            <Route path="/admin/AddChapter" element={<Chapters />} />
-            <Route path="/admin/feedback" element={<FeedBack />} />
-            <Route path="/admin/Donation" element={<DonationList />} />
-            <Route path="/admin/transcript" element={<RequestedTranscript />} />
-            <Route path="/admin/donation/:id" element={<EditDonation />} />
-            <Route path="/admin/AddDonation" element={<AddDonation />} />
+            <Route path="home" element={<Home />} />
+            <Route path="users" element={role === "systemAdmin" && <Users />} />
+            <Route path="News" element={<NewsList />} />
+            <Route path="News/edit-news/:newsId" element={<Editnews />} />
+            <Route path="AddNews" element={<News />} />
+            <Route path="AddUser" element={<AddUser />} />
+            <Route path="story" element={<Stories />} />
+            <Route path="addedStories" element={<AddedStory />} />
+            <Route path="Events" element={<EventList />} />
+            <Route path="adminEvents/:id" element={<EditEvent />} />
+            <Route path="admin/gallery/:galleryID" element={<AGallery />} />
+            <Route path="gallery" element={<GalleryList />} />
+            <Route path="edit-gallery/:galleryID" element={<EditGallery />} />
+            <Route path="Addgallery" element={<AddGallery />} />
+            <Route path="AddEvent" element={<AddEvent />} />
+            <Route path="jobOffer" element={<JobOffer />} />
+            <Route path="SuggestedJob" element={<SuggestedJob />} />
+            <Route path="chapters" element={<ChaptersList />} />
+            <Route path="chapters/chapters/:chapterId" element={<EditClub />} />
+            <Route path="AddChapter" element={<Chapters />} />
+            <Route path="feedback" element={<FeedBack />} />
+            <Route path="Donation" element={<DonationList />} />
+            <Route path="transcript" element={<RequestedTranscript />} />
+            <Route path="donation/:id" element={<EditDonation />} />
+            <Route path="AddDonation" element={<AddDonation />} />
           </Routes>
         </div>
       ) : (
