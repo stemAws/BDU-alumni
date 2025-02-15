c03e6687085936e2fcb992bede1202e0b87ae629
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-15 18:32:55 +0300

    changed component to element

--- client/BDU-Almuni-FrontEnd/src/App.jsx
+++ client/BDU-Almuni-FrontEnd/src/App.jsx
@@ -1,18 +1,17 @@
-import MainPage from './pages/MainPage'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Admin from './pages/Admin'
+import MainPage from "./pages/MainPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Admin from "./pages/Admin";
 const App = () => {
   return (
     <>
-<Router >
-<Routes>
-<Route path="*" exact Component = {MainPage} />
-<Route path="/admin/*" exact Component = {Admin} />
-</Routes>
-</Router>
-
+      <Router>
+        <Routes>
+          <Route path="*" element={<MainPage />} />
+          <Route path="/admin/*" element={<Admin />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
