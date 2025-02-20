ef87cb8426b251fb7cdeb2f6ae031dd429bfec3a
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-20 16:09:23 +0300

    admin signin route

--- client/BDU-Almuni-FrontEnd/src/App.jsx
+++ client/BDU-Almuni-FrontEnd/src/App.jsx
@@ -1,12 +1,14 @@
 import MainPage from "./pages/MainPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
+import AdminSignin from "./component/AdminSignin";
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
           <Route path="*" element={<MainPage />} />
+          <Route path="/admin/signin" element={<AdminSignin />} />
           <Route path="/admin/*" element={<Admin />} />
         </Routes>
       </Router>
