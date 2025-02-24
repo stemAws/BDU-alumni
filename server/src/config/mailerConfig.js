bd42edc402b8a2ac03c02d9815e9d725f6df5270
NetsanetAlemu <netsialemu1907@gmail.com>
2025-02-24 12:23:12 +0300

    added new config

--- server/src/config/mailerConfig.js
+++ server/src/config/mailerConfig.js
@@ -1,15 +1,25 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD
+//   }
+// });
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  service: 'gmail',
+  port: 465, // Use 465 if you set `secure: true`
+  secure: true, // true for port 465, false for 587
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
+    user: process.env.G_MAIL_USER,
+    pass: process.env.G_MAIL_PASSWORD, // Use App Password here
+  },
 });
 
 module.exports = transporter;
