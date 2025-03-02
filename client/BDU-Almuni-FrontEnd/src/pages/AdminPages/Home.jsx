c6b32670caa95d15dc0dee2e8b017533ce57ce59
NetsanetAlemu <netsialemu1907@gmail.com>
2025-03-03 01:13:41 +0300

    remove log

--- client/BDU-Almuni-FrontEnd/src/pages/AdminPages/Home.jsx
+++ client/BDU-Almuni-FrontEnd/src/pages/AdminPages/Home.jsx
@@ -2,8 +2,6 @@ import "../../styles/home.css";
 // import Users from './Users'
 import Charts from "./Charts";
 const home = () => {
-  console.log("Home component rendered");
-
   return (
     <div className="home">
       <Charts />
